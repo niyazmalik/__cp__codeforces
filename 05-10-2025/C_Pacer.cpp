cd96d36aab74c801f1c1a847b7038344d36c9ed0
niyazmalik <your_email_used_on_niyazmalik@whatever.com>
2025-10-07 19:29:59 +0530

    should have used XOR for side change checks in prob C_Pacer

--- 05-10-2025/C_Pacer.cpp
+++ 05-10-2025/C_Pacer.cpp
@@ -7,32 +7,45 @@ using ll = long long;
 void run() {
     int n, m;
     cin >> n >> m;
-    int ans = 0;
-    int prev = -1;
-    bool start = true;
+    // int ans = 0;
+    // int prev = -1;
+    // bool start = true;
+    // for(int i = 0; i < n; i++) {
+    //     int a, b;
+    //     cin >> a >> b;
+    //     int t = a - (i ? prev : 0);
+    //     if(start) {
+    //         if(b) {
+    //             ans += 1 + t - 1 - (t - 1) % 2;
+    //             start = !start;
+    //         } else {
+    //             ans += t - t % 2;
+    //         }
+    //     } else {
+    //         if(!b) {
+    //             ans += 1 + t - 1 - (t - 1) % 2;
+    //             start = !start;
+    //         } else {
+    //             ans += t - t % 2;
+    //         }
+    //     }
+    //     prev = a;
+    // }
+    // int remaining = m - prev;
+    // cout << ans + remaining << endl;
+
+    // shoud have used xor for side change checkssss...
+    int ans = m;
+    int preva = 0, prevb = 0;
     for(int i = 0; i < n; i++) {
         int a, b;
         cin >> a >> b;
-        int t = a - (i ? prev : 0);
-        if(start) {
-            if(b) {
-                ans += 1 + t - 1 - (t - 1) % 2;
-                start = !start;
-            } else {
-                ans += t - t % 2;
-            }
-        } else {
-            if(!b) {
-                ans += 1 + t - 1 - (t - 1) % 2;
-                start = !start;
-            } else {
-                ans += t - t % 2;
-            }
-        }
-        prev = a;
+        int t = a - preva;
+        if((b^prevb && !(t & 1)) || (!(b^prevb) && (t & 1))) ans--;
+        prevb = b;
+        preva = a;
     }
-    int remaining = m - prev;
-    cout << ans + remaining << endl;
+    cout << ans << endl;
 }
 
 signed main() {
